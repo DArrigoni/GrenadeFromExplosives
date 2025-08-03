8b9cf4ec2154ec73c63bce8cc753c88af9144c9b
Drew Arrigoni <arrigoni.andrew@gmail.com>
2025-08-03 08:52:07 -0400

    Initial commit

---
+++ GrenadeFromExplosives_1.0.0/data.lua
@@ -0,0 +1,44 @@
+data:extend({
+  {
+    type = "recipe",
+    name = "grenade-from-explosive",
+    category = "crafting",
+    enabled = false,
+    energy_required = 1,
+    ingredients = {
+      { type = "item", name = "explosives", amount = 10 },
+      { type = "item", name = "steel-plate", amount = 1 }
+    },
+    results = {
+      { type = "item", name = "grenade", amount = 1 }
+    },
+    main_product = "grenade",
+    allow_as_intermediate = true
+  }
+})
+
+data:extend({
+  {
+    type = "technology",
+    name = "grenade-from-explosives",
+    icon = "__GrenadeFromExploisves__/graphics/icons/grenade_from_explosives.png",
+    icon_size = 64,
+    icon_mipmaps = 1,
+    prerequisites = { "explosives" },
+    unit = {
+      count = 50,
+      ingredients = {
+        { "automation-science-pack", 1 },
+        { "logistic-science-pack", 1 }
+      },
+      time = 15
+    },
+    effects = {
+      {
+        type = "unlock-recipe",
+        recipe = "grenade-from-explosive"
+      }
+    },
+    order = "e-a-bb"
+  }
+})
